ac2e8ad1828d7b47e08da0cb15bb3ca26f916832
aleclarson <alec.stanford.larson@gmail.com>
2018-04-09 15:37:52 -0400

    client: major changes
    when NODE_ENV == 'test'
      - set sock._reset for use between tests
      - set sock._req to the ClientRequest object
      - set sock._res to the IncomingMessage object
      - set stream._reset for removing our listeners
    
    - emit a 'connecting' event
    - set sock.id once connected instead of before
    - rename sock.disconnect() to sock.close()
    - rename 'disconnect' event to 'close'
    - emit 'close' event *after* calling `reconnect`
    - make sock.close abort reconnect attempts
    - avoid emitting 'close' event on connect error
    - handle reconnect errors properly
    - reset the retry count if user aborts
    - cancel the retry timer properly
    - close the socket path watcher if user aborts
    - remove console.log calls

--- client/sock.js
+++ client/sock.js
@@ -9,13 +9,22 @@ let {
   SOCK_PATH,
 } = require('../server/paths')
 
+let noop = Function.prototype
+let isTest = process.env.NODE_ENV == 'test'
+let eventRE = /^([^\n]+)\n(.+)\n\n$/
+let offlineRE = /^(ENOENT|ECONNREFUSED)$/
 let SOCK_NAME = path.relative(WCH_DIR, SOCK_PATH)
 
 let sock = quest.sock(SOCK_PATH)
 module.exports = sock
 
+// `stream` exists when connecting or connected
 let stream = null
+
+// `watcher` exists when waiting for server to start
 let watcher = null
+
+// `connecting` exists when connection is imminent
 let connecting = null
 
 sock.connected = false
@@ -29,79 +38,121 @@ sock.connect = function() {
   return connecting || (connecting = new Promise(connect))
 }
 
-let abortErr = new Error('Aborted by user')
-sock.disconnect = function() {
-  if (stream) {
+sock.close = function() {
+  if (isReconnecting()) {
+    abortReconnect()
+    connecting = null
+  } else if (stream) { // Closing while connected
     sock.connected = false
     stream.destroy()
   }
 }
 
 sock.on = events.on
-
-let noop = Function.prototype
-let eventRE = /^([^\n]+)\n(.+)\n\n$/
-let offlineRE = /^(ENOENT|ECONNREFUSED)$/
-function connect(resolve, reject) {
-  stream = sock.stream('/events', {
-    'x-client-id': sock.id = uuid(),
-  })
-  stream.on('response', () => {
-    if (stream.ok) {
-      connecting = null
-      sock.connected = true
-      resolve()
-      events.emit('connect')
-    }
-  })
-  stream.setEncoding('utf8')
-  stream.on('data', (event) => {
-    if (event == null) return
-    let [id, args] = eventRE.exec(event).slice(1)
-    console.log(id + ': ' + args)
-    events.emit(id, JSON.parse(args))
-  }).on('error', (err) => {
-    if (offlineRE.test(err.code)) {
-      console.log('server offline')
-      reconnect(resolve, reject)
-    } else if (connecting) {
-      stream = null
-      connecting = null
-      reject(err)
-    } else {
-      events.emit('error', err)
-    }
-  }).on('end', () => {
-    if (sock.connected) {
-      sock.connected = false
-      events.emit('disconnect')
-      connecting = new Promise(reconnect)
-      connecting.catch(noop)
-    } else {
-      stream = null
-      clearTimeout(retryTimer)
-      if (watcher) {
-        watcher.close()
-        watcher = null
-      }
-      if (connecting) {
+if (isTest) {
+  sock._reset = function() {
+    events.clear()
+    if (stream) {
+      stream._reset()
+      if (this._req.socket.destroyed) {
+        abortReconnect()
         connecting = null
-        reject(abortErr)
-      } else {
-        sock.connected = false
-        events.emit('disconnect')
+        return
       }
+      return new Promise(resolve => {
+        this._req.on('close', resolve)
+        return this.close()
+      })
     }
-  })
+  }
 }
 
-let retries = 0, retryTimer
+function connect(resolve, reject) {
+  let id = uuid()
+  let req = sock.request('GET', '/events', {
+    'x-client-id': id,
+  })
+  if (isTest) {
+    sock._req = req
+  }
+
+  stream = quest.stream(req)
+
+  stream.on('connect', connected)
+  function connected() {
+    sock.id = id
+    sock.connected = true
+    if (isTest) {
+      sock._res = req.res
+    }
+    connecting = null
+    resolve()
+    events.emit('connect')
+  }
+
+  stream.setEncoding('utf8')
+  stream.on('data', emit)
+  function emit(event) {
+    let [id, args] = eventRE.exec(event).slice(1)
+    events.emit(id, JSON.parse(args))
+  }
+
+  stream.on('close', close)
+  function close() {
+    if (sock.connected) {
+      sock.connected = false
+      connecting = new Promise(reconnect)
+      connecting.catch(noop)
+      events.emit('close')
+    } else { // Closed by user
+      abortReconnect()
+      if (connecting) {
+        connecting = null
+        reject(CloseError())
+      } else { // Closed while connected
+        events.emit('close')
+      }
+    }
+  }
+
+  stream.on('error', uhoh)
+  function uhoh(err) {
+    if (connecting) {
+      stream.removeListener('close', close)
+      if (offlineRE.test(err.code)) {
+        reconnect(resolve, reject)
+      } else {
+        reject(err)
+        stream.destroy()
+        if (retries == 0) { // not a reconnect
+          stream = null
+          connecting = null
+        }
+      }
+    } else {
+      events.emit('error', [err])
+    }
+  }
+
+  if (isTest) {
+    stream._reset = function() {
+      this.removeListener('connect', connected)
+      this.removeListener('data', emit)
+      this.removeListener('close', close)
+      this.removeListener('error', uhoh)
+    }
+  }
+
+  events.emit('connecting')
+}
+
+let retries = 0, retryId
 function reconnect(resolve, reject) {
   if (fs.exists(SOCK_PATH)) {
     let fuzz = 1.25 - 0.5 * Math.random()
     let delay = fuzz * 300 * Math.pow(2.2, ++retries)
-    console.log('Reconnecting in ' + delay + ' ms...')
-    let retryTimer = setTimeout(async () => {
+    retryId = setTimeout(async () => {
+      retryId = null
       try {
         await new Promise(connect)
         retries = 0
@@ -109,19 +160,45 @@ function reconnect(resolve, reject) {
       } catch(err) {
         if (stream) {
           reconnect(resolve, reject)
-        } else reject(err)
+        } else { // Closed by user
+          retries = 0
+          reject(err)
+        }
       }
     }, delay)
   } else {
-    console.log('Watching socket path...')
     watcher = fs.watch(WCH_DIR, (evt, file) => {
       if (file == SOCK_NAME) {
         watcher.close()
         watcher = null
-        if (stream) {
-          reconnect(resolve, reject)
-        } else reject(abortErr)
+        reconnect(resolve, reject)
       }
     })
+    let {close} = watcher
+    watcher.close = function() {
+      close.apply(this, arguments)
+      if (!stream) { // Closed by user
+        reject(CloseError())
+      }
+    }
   }
+}
+
+function isReconnecting() {
+  return (connecting && (retryId || watcher)) != null
+}
+
+function abortReconnect() {
+  stream = null
+  clearTimeout(retryId)
+  if (watcher) {
+    watcher.close()
+    watcher = null
+  }
+}
+
+function CloseError() {
+  let err = Error('Closed by user')
+  err.code = 'ECONNRESET'
+  return err
 }
