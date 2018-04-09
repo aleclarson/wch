63b6ade6ba7ef6d1fd391c33ffe2e4d6b9c2748f
aleclarson <alec.stanford.larson@gmail.com>
2018-04-09 16:04:17 -0400

    use tmpdir for test server

--- server/paths.js
+++ server/paths.js
@@ -1,6 +1,11 @@
 let os = require('os')
 
-let WCH_DIR = os.homedir() + '/.wch'
+let WCH_DIR
+if (process.env.NODE_ENV == 'test') {
+  WCH_DIR = os.tmpdir() + '/.wch'
+} else {
+  WCH_DIR = os.homedir() + '/.wch'
+}
 
 module.exports = {
   WCH_DIR,
