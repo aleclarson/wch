2e2d05728b4bb713222ab8a836ca94e42bcbee25
aleclarson <alec.stanford.larson@gmail.com>
2018-07-05 15:03:46 -0400

    feat: add offline queue
    All methods may be used while the client is not connected.
    
    All actions are always added to the offline queue. If connected, the queue is processed on the next tick of the event loop. Otherwise, it waits until the client reconnects. This makes methods more resilient against disconnects, but disconnects in between the request and response may still cause errors.
    
    Some actions (track, untrack, unsubscribe) will block the queue until they are resolved or rejected. This helps prevent race conditions.

--- index.js
+++ index.js
@@ -1,111 +1,134 @@
-let {Readable} = require('readable-stream')
-let wchQuery = require('./lib/query')
-let events = require('./events')
-let quest = require('quest')
-let noop = require('noop')
-let path = require('path')
-let log = require('lodge').debug('wch')
-let cp = require('child_process')
-let fs = require('fsx')
+const {Readable} = require('readable-stream')
+const wchQuery = require('./lib/query')
+const Runner = require('./lib/runner')
+const events = require('./lib/events')
+const quest = require('quest')
+const path = require('path')
+const sock = require('./lib/sock')
+const log = require('lodge').debug('wch')
+const cp = require('child_process')
 
-let {SOCK_PATH} = require('./paths')
+const {SOCK_PATH} = require('./paths')
 
-let sock = require('./sock')
-let request = sock.request.bind(sock)
+const noop = () => {}
+const success = (res) => !res || !res.error
 
-// Watch a root.
-async function wch(root) {
-  if (!fs.exists(SOCK_PATH))
-    throw notStarted()
-  let req = request('PUT', '/roots')
-  return quest.json(req, {root}).then(success)
-}
+const {run} = new Runner({
+  // Track a directory indefinitely.
+  track(root) {
+    const req = sock.request('PUT', '/roots')
+    return quest.json(req, {root}).then(success)
+  },
+  // Stop tracking a directory.
+  untrack(root) {
+    const req = sock.request('DELETE', '/roots')
+    return quest.json(req, {root}).then(success)
+  },
+  // Subscribe to matching changes in a directory.
+  subscribe(dir, query) {
+    const req = sock.request('POST', '/watch', {
+      'x-client-id': sock.id,
+    })
+    return quest.json(req, {dir, query})
+  },
+  // Destroy a subscription.
+  unsubscribe(id) {
+    const req = sock.request('POST', '/unwatch')
+    return quest.send(req, {id})
+  },
+  // Find matching paths in a directory.
+  query(dir, query) {
+    const req = sock.request('GET', '/query')
+    return quest.json(req, {dir, query})
+  },
+  // Fetch the directories being tracked.
+  list() {
+    return sock.json('/roots')
+  }
+})
 
-// Unwatch a root.
-wch.unwatch = async function(root) {
-  if (!fs.exists(SOCK_PATH))
-    throw notStarted()
-  let req = request('DELETE', '/roots')
-  return quest.json(req, {root}).then(success)
+function wch(...args) {
+  return run('track', args)
 }
 
 wch.connect = sock.connect
+wch.on = events.on.bind(events)
 
-// Plugin events
-wch.on = function(evt, fn) {
-  return events.on(evt, fn)
+wch.unwatch = function(...args) {
+  return run('untrack', args)
 }
 
-// File event streaming
-wch.stream = function(root, opts) {
-  if (opts && typeof opts.since == 'object') {
-    opts.since = Math.floor(opts.since.getTime() / 1000)
+wch.stream = function(dir, query = {}) {
+  if (query && typeof query.since == 'object') {
+    query.since = Math.floor(query.since.getTime() / 1000)
   }
 
-  let rewatcher = null
-  let stream = new Readable({
+  const stream = new Readable({
     read: noop, // Push only
     objectMode: true,
-    destroy,
+    async destroy(err, done) {
+      rewatcher.dispose()
+      done(err)
+
+      // 'readable-stream' does not emit "close" by default
+      process.nextTick(() => stream.emit('close'))
+    }
   })
 
-  let watching = watch()
-  watching.catch(fatal)
-  return stream
+  if (sock.connected) watch()
+  const rewatcher = events.on('connect', watch)
 
-  async function watch() {
+  let watcher
+  function watch() {
+    const init = run('subscribe', [dir, query])
+    log('stream:subscribe', init.action)
 
-    // Rewatch on server restart.
-    rewatcher = events.on('connect', () => {
-      rewatcher.dispose()
-      watching = watch()
-      watching.catch(fatal)
+    init.then(info => {
+      if (stream.destroyed) return
+      watcher = events.on(info.id, push)
+    }).catch(err => {
+      log('stream:error', err)
+      stream.destroyed || stream.destroy(err)
     })
 
-    // Setup the watch subscription.
-    let req = request('POST', '/watch', {
-      'x-client-id': sock.id,
+    // This watcher is scrapped if the connection is lost.
+    events.on('close', function onDisconnect() {
+      if (watcher) watcher.dispose()
+
+      events.off('close', onDisconnect)
+      stream.off('close', onDestroy)
     })
 
-    log('subscribe:', log.lblue(root))
-    let {id} = await quest.json(req, {root, opts})
-    return events.on(id, (file) => {
-      stream.push(file)
-
-      // The watch root was deleted!
-      if (file.name == '/') {
-        stream.destroy()
-      }
-    })
-  }
-  async function destroy(err, done) {
-    if (rewatcher) rewatcher.dispose()
-    try {
-      let watcher = await watching
-      watcher.dispose()
-      if (sock.connected) {
-        let req = request('POST', '/unwatch')
-        quest.send(req, {
-          id: watcher.id,
-        }).catch(noop)
-      }
-    } catch(e) {}
-    done(err)
-
-    // 'readable-stream' does not emit "close" by default
-    process.nextTick(() => stream.emit('close'))
-  }
-  function fatal(err) {
-    if (!stream.destroyed) {
-      stream.destroy(err)
+    stream.on('close', onDestroy)
+    function onDestroy() {
+      log('stream:close', init.action)
+      // Cancel the outgoing request (if possible).
+      init.action.cancel()
+      // The incoming response may never come, but this is
+      // the best way to ensure the watch ends gracefully.
+      init.then(info => {
+        if (watcher) watcher.dispose()
+        run('unsubscribe', [info.id]).catch(noop)
+        log('stream:unsubscribe', info)
+      })
     }
   }
+
+  function push(file) {
+    log('stream:push', file)
+    stream.push(file)
+
+    // The watch root was deleted!
+    if (file.name == '/') {
+      stream.destroy()
+    }
+  }
+
+  return stream
 }
 
-// File queries.
-wch.query = function(root, query) {
-  let req = request('GET', '/query')
-  return quest.json(req, {root, query})
+wch.query = function(...args) {
+  return run('query', args)
 }
 
 wch.expr = function(query) {
@@ -113,47 +136,7 @@ wch.expr = function(query) {
 }
 
 wch.list = async function() {
-  if (!fs.exists(SOCK_PATH)) throw notStarted()
-  return (await sock.json('/roots')).roots
+  return (await run('list')).roots
 }
 
 module.exports = wch
-
-function success(res) {
-  return !res || !res.error
-}
-
-// A stream that emits "error" upon read.
-function estream(err) {
-  return Readable({
-    read() {
-      this.emit('error', err)
-    }
-  })
-}
-
-function notStarted() {
-  let err = Error('Run `wch start` first')
-  err.code = 408
-  return err
-}
-
-function parseJsonStream(stream) {
-  let {push} = stream
-  stream.push = function(val, enc) {
-    if (val) {
-      if (Buffer.isBuffer(val)) {
-        val = val.toString()
-      }
-      try {
-        val = JSON.parse(val)
-      } catch(err) {
-        err.json = val
-        return stream.emit('error', err)
-      }
-    }
-    return push.call(this, val)
-  }
-  stream._readableState.objectMode = true
-  return stream
-}
